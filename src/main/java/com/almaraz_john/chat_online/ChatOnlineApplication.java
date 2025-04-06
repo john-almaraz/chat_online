66df869e1e763812ee7e0355d3453c7ece914741
john-almaraz <125034147+john-almaraz@users.noreply.github.com>
2025-04-06 19:37:18 +0200

    first commit

---
+++ src/main/java/com/almaraz_john/chat_online/ChatOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.almaraz_john.chat_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almaraz_john/chat_online/ChatOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almaraz_john.chat_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
